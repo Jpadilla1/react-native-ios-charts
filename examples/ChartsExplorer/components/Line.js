0ee950b937d5adcc06b3d80997ddba6e8d18daba
Jpadilla1 <jpadilla1293@gmail.com>
2016-03-15 09:39:13 -0700

    fixed line chart in charts explorer

--- examples/ChartsExplorer/components/Line.js
+++ examples/ChartsExplorer/components/Line.js
@@ -5,56 +5,6 @@ import React, {
 
 import { LineChart } from 'react-native-ios-charts';
 
-const config = {
-  dataSets: [{
-    values: [-1, 1, -1],
-    drawValues: false,
-    colors: ['rgb(199, 255, 140)'],
-    label: 'Sine function',
-    drawCubic: true,
-    drawCircles: false,
-    lineWidth: 2
-  }, {
-    values: [1, -1, 1],
-    drawValues: false,
-    colors: ['rgb(255, 247, 141)'],
-    label: 'Cosine function',
-    drawCubic: true,
-    drawCircles: false,
-    lineWidth: 2
-  }],
-  backgroundColor: 'transparent',
-  labels: ['Jan', 'Feb', 'Mar'],
-  minOffset: 20,
-  scaleYEnabled: false,
-  legend: {
-    textSize: 12
-  },
-  xAxis: {
-    axisLineWidth: 0,
-    drawLabels: false,
-    position: 'bottom',
-    drawGridLines: false
-  },
-  leftAxis: {
-    customAxisMax: 1,
-    customAxisMin: -1,
-    labelCount: 11,
-    startAtZero: false,
-    spaceTop: 0.1,
-    spaceBottom: 0.1
-  },
-  rightAxis: {
-    enabled: false,
-    drawGridLines: false
-  },
-  valueFormatter: {
-    minimumSignificantDigits: 1,
-    type: 'regular',
-    maximumDecimalPlaces: 1
-  }
-};
-
 
 const styles = StyleSheet.create({
   container: {
@@ -68,6 +18,55 @@ export default class Line extends Component {
   static displayName = 'Line';
 
   render() {
+    const config = {
+      dataSets: [{
+        values: [-1, 1, -1],
+        drawValues: false,
+        colors: ['rgb(199, 255, 140)'],
+        label: 'Sine function',
+        drawCubic: true,
+        drawCircles: false,
+        lineWidth: 2
+      }, {
+        values: [1, -1, 1],
+        drawValues: false,
+        colors: ['rgb(255, 247, 141)'],
+        label: 'Cosine function',
+        drawCubic: true,
+        drawCircles: false,
+        lineWidth: 2
+      }],
+      backgroundColor: 'transparent',
+      labels: ['Jan', 'Feb', 'Mar'],
+      minOffset: 20,
+      scaleYEnabled: false,
+      legend: {
+        textSize: 12
+      },
+      xAxis: {
+        axisLineWidth: 0,
+        drawLabels: false,
+        position: 'bottom',
+        drawGridLines: false
+      },
+      leftAxis: {
+        customAxisMax: 1,
+        customAxisMin: -1,
+        labelCount: 11,
+        startAtZero: false,
+        spaceTop: 0.1,
+        spaceBottom: 0.1
+      },
+      rightAxis: {
+        enabled: false,
+        drawGridLines: false
+      },
+      valueFormatter: {
+        minimumSignificantDigits: 1,
+        type: 'regular',
+        maximumDecimalPlaces: 1
+      }
+    };
     return (
       <LineChart config={config} style={styles.container}/>
     );
